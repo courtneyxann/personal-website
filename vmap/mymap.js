6840b7ca65f88f368020c17ef165fb8313d14ad1
courtneyxann <courtney.heitman@gmail.com>
2017-06-26 20:21:43 -0500

    Adding initial state capitol page to track which ones I've been to.

---
+++ vmap/mymap.js
@@ -0,0 +1,28 @@
+$('#vmap').vectorMap(
+{
+    map: 'usa_en',
+    backgroundColor: '#ffffff',
+    borderColor: '#ffffff',
+    borderOpacity: 0.25,
+    borderWidth: 1,
+    color: '#cccccc',
+    enableZoom: false,
+    hoverColor: null,
+    hoverColors: {
+        ne: '#ba8cbf',
+        tx: '#ba8cbf',
+        tn: '#ba8cbf',
+        az: '#ba8cbf'
+    },
+    hoverOpacity: null,
+    normalizeFunction: 'linear',
+    showTooltip: false,
+    showLabels: false,
+    selectedColor: null,
+    colors: {
+        ne: '#781D7E',
+        tx: '#781D7E',
+        tn: '#781D7E',
+        az: '#781D7E'
+    }
+});
